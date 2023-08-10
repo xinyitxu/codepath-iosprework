a66f4d38601d34915b5bfd4cfdefa16e583d0dcc
xinyitxu <mxtchamilkteas@gmail.com>
2023-08-10 14:48:44 -0400

    update

---
+++ CodePath Project/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CodePath Project
+//
+//  Created by Xin Yi Therese Xu on 7/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+
+}
+
